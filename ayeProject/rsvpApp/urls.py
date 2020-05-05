5c008ff13a933206ac98e96f70a3f8dedc0d6547
marcoburgos <marko_burgos@gmail.com>
2020-05-05 16:00:50 -0400

    ajax post call return 200 code

--- ayeProject/rsvpApp/urls.py
+++ ayeProject/rsvpApp/urls.py
@@ -3,6 +3,5 @@ from rsvpApp import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('validate_guest/', views.validate_guest, name='email_form_view'),
-    path('save_guest_into_db/', views.save_guest_into_db, name='guests_form_view')
+    path('ajax_call/', views.ajax_call, name='email_form_view')
  ]
--- ayeProject/rsvpApp/views.py
+++ ayeProject/rsvpApp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import JsonResponse
 from . import forms
 from rsvpApp.models import Guest, Invitation
+from datetime import datetime, timedelta
 
 
 # Create your views here.
@@ -12,25 +13,38 @@ def index(request):
     guests_form = forms.GuestsForm()
     return render(request, 'rsvpApp/index.html', {'email_form': email_form, 'guests_form': guests_form})
 
-def validate_guest(request):
-    email = request.GET.get('email', None)
-    data = {
-         'is_there': Guest.objects.filter(email__exact=email).exists()
-     }
-    if data['is_there']:
-        emailID = Guest.objects.get(email__exact=email)
-        data['guests'] = Invitation.objects.get(invitation_owner_name = emailID).guest_names
-        data['name'] = Guest.objects.get(email = email).invitation_owner_name
-        data['no_of_guests'] = Invitation.objects.get(invitation_owner_name = emailID).number_of_guests
+def ajax_call(request):
+    if request.method == 'GET':
+        email = request.GET.get('email', None)
+        data = {
+             'is_there': Guest.objects.filter(email__exact=email).exists()
+         }
+        if data['is_there']:
+            emailID = Guest.objects.get(email__exact=email)
+            data['guests'] = Invitation.objects.get(invitation_owner_name = emailID).guest_names
+            data['name'] = Guest.objects.get(email = email).invitation_owner_name
+            data['no_of_guests'] = Invitation.objects.get(invitation_owner_name = emailID).number_of_guests
+
+        else:
+            data['error_message'] = 'No existe el correo ingresado en la base de datos.'
+
+
+        print(str(data))
+
+        return JsonResponse(data)
 
     else:
-        data['error_message'] = 'No existe el correo ingresado en la base de datos.'
+        names_to_saved = request.POST.get('guests', None)
+        invitation_size = names_to_saved.split(",")
+        email = request.POST.get('email', None)
+        emailID = Guest.objects.get(email__exact=email)
+
+        guestRecord = Invitation.objects.get(invitation_owner_name__exact=emailID)
+        guestRecord.guests_confirmed = names_to_saved
+        guestRecord.total_guests_confirmed = len(invitation_size)
+        guestRecord.is_RSVP = True
+        guestRecord.date_RSVP = (datetime.now()) - (timedelta(hours=4, minutes=00))
+        guestRecord.save()
 
 
-    print(str(data))
-
-    return JsonResponse(data)
-
-def save_guest_into_db(request):
-     names_to_saved = request.GET.get('guests', None)
-     print(names_to_saved)
+        return HttpResponse("Success!")
