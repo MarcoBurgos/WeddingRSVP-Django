9b1d15f43ddf3bba9024f32c793e467dd69d3d77
marcoburgos <marko_burgos@gmail.com>
2020-05-05 19:53:20 -0400

    admin templae for list and filter

--- ayeProject/rsvpApp/admin.py
+++ ayeProject/rsvpApp/admin.py
@@ -4,7 +4,14 @@ from rsvpApp.models import Guest, Invitation
 # Register your models here.
 
 class InvitationForm(admin.ModelAdmin):
-    exclude = ('guests_confirmed', 'total_guests_confirmed', 'is_RSVP', 'date_RSVP',)
+
+    fields = ['invitation_owner_name', 'guest_names', 'number_of_guests', 'guests_confirmed', 'total_guests_confirmed', 'is_RSVP', 'date_RSVP']
+    #exclude = ('guests_confirmed', 'total_guests_confirmed', 'is_RSVP', 'date_RSVP',)
+    #search_fields = ['invitation_owner_name']
+
+    list_filter = ['is_RSVP']
+
+    list_display = ['invitation_owner_name', 'guest_names', 'number_of_guests', 'guests_confirmed', 'total_guests_confirmed', 'is_RSVP', 'date_RSVP']
 
 
 
--- ayeProject/rsvpApp/models.py
+++ ayeProject/rsvpApp/models.py
@@ -18,10 +18,10 @@ class Invitation(models.Model):
     verbose_name="Seleccione el invitado con el cual relacionar la invitación")
     guest_names = models.CharField(max_length=256)
     number_of_guests = models.IntegerField()
-    guests_confirmed = models.CharField(max_length=256, null=True)
-    total_guests_confirmed = models.IntegerField(null=True)
-    is_RSVP = models.BooleanField(null=True)
-    date_RSVP =models.DateField(null=True)
+    guests_confirmed = models.CharField(max_length=256, null=True, blank=True)
+    total_guests_confirmed = models.IntegerField(null=True, blank=True)
+    is_RSVP = models.BooleanField(null=True, blank=True)
+    date_RSVP =models.DateField(null=True, blank=True)
 
     def __str__(self):
         return str(self.guest_names)
